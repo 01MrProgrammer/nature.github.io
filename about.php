d1c89ee908bfa044bc00ee83b9195a71f88b2fc2
Mr. Programmer <101489543+01MrProgrammer@users.noreply.github.com>
2023-10-15 22:52:19 +0530

    Nature Web
    web

---
+++ about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="CSS/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400&display=swap" rel="stylesheet">
+	<title></title>
+</head>
+<body>
+
+<?php include 'menu.php'; ?>
+
+<div class="jumbotron">
+  <h1>Nature :)</h1>
+  <p>Nature is a beautiful part of our life...</p>
+</div>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="images/cc4.jpg" class="img-fluid aboutimg">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+      <h2 class="display-4">Experience Natural beauty.</h2>
+      <p class="py-3">Natural beauty.</p>
+      <a href="about.php" class="btn btn-success">check More</a>
+    </div>
+  </div>
+  </div>
+</section>
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@Nature</p>
+</footer>
+
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="CSS/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400&display=swap" rel="stylesheet">
+  <title></title>
+</head>
+<body>
+
+<?php include 'menu.php'; ?>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Our services</h2>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+  <img class="card-img-top" src="images/cc5.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Beautiful Nature :)</h4>
+    <p class="card-text">Some Pic's</p>
+    <a href="#" class="btn btn-primary">Check...</a>
+  </div>
+</div>
+      
+      </div>
+
+       <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+  <img class="card-img-top" src="images/cc5.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Beautiful Nature :)</h4>
+    <p class="card-text">Some Pic's</p>
+    <a href="#" class="btn btn-primary">Check...</a>
+  </div>
+</div>
+      
+      </div>
+
+
+       <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+  <img class="card-img-top" src="images/cc5.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Beautiful Nature :)</h4>
+    <p class="card-text">Some Pic's</p>
+    <a href="#" class="btn btn-primary">Check...</a>
+  </div>
+</div>
+      
+      </div>
+
+
+    </div>
+  </div>
+</section>
+
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@Nature</p>
+</footer>
+
+</body>
+</html>
